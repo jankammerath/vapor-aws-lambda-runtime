29c23e2b787dc68c260e37baa2f78cc2a876a050
Jan Kammerath <jankammerath@gmail.com>
2023-12-04 16:11:09 +0100

    Update APIGateway.swift

--- Sources/VaporAWSLambdaRuntime/APIGateway.swift
+++ Sources/VaporAWSLambdaRuntime/APIGateway.swift
@@ -1,16 +1,45 @@
 import AWSLambdaEvents
-import AWSLambdaRuntime
+import AWSLambdaRuntimeCore
 import ExtrasBase64
 import NIO
 import NIOHTTP1
 import Vapor
+
+// MARK: - Handler -
+
+struct APIGatewayHandler: EventLoopLambdaHandler {
+    typealias In = APIGateway.Request
+    typealias Out = APIGateway.Response
+
+    private let application: Application
+    private let responder: Responder
+
+    init(application: Application, responder: Responder) {
+        self.application = application
+        self.responder = responder
+    }
+
+    public func handle(context: Lambda.Context, event: APIGateway.Request)
+        -> EventLoopFuture<APIGateway.Response>
+    {
+        let vaporRequest: Vapor.Request
+        do {
+            vaporRequest = try Vapor.Request(req: event, in: context, for: self.application)
+        } catch {
+            return context.eventLoop.makeFailedFuture(error)
+        }
+
+        return self.responder.respond(to: vaporRequest)
+            .map { APIGateway.Response(response: $0) }
+    }
+}
 
 // MARK: - Request -
 
 extension Vapor.Request {
     private static let bufferAllocator = ByteBufferAllocator()
 
-    convenience init(req: APIGatewayRequest, in ctx: LambdaContext, for application: Application) throws {
+    convenience init(req: APIGateway.Request, in ctx: Lambda.Context, for application: Application) throws {
         var buffer: NIO.ByteBuffer?
         switch (req.body, req.isBase64Encoded) {
         case (let .some(string), true):
@@ -43,23 +72,17 @@ extension Vapor.Request {
             on: ctx.eventLoop
         )
 
-        storage[APIGatewayRequestStorageKey.self] = req
+        storage[APIGateway.Request.self] = req
     }
 }
 
-fileprivate struct APIGatewayRequestStorageKey: StorageKey {
-    typealias Value = APIGatewayRequest
-}
-
-extension Request {
-    public var apiGatewayRequest: APIGatewayRequest? {
-        storage[APIGatewayRequestStorageKey.self]
-    }
+extension APIGateway.Request: Vapor.StorageKey {
+    public typealias Value = APIGateway.Request
 }
 
 // MARK: - Response -
 
-extension APIGatewayResponse {
+extension APIGateway.Response {
     init(response: Vapor.Response) {
         var headers = [String: [String]]()
         response.headers.forEach { name, value in
@@ -69,7 +92,7 @@ extension APIGatewayResponse {
         }
 
         if let string = response.body.string {
-            self = APIGatewayResponse(
+            self = APIGateway.Response(
                 statusCode: AWSLambdaEvents.HTTPResponseStatus(code: response.status.code),
                 multiValueHeaders: headers,
                 body: string,
@@ -77,14 +100,14 @@ extension APIGatewayResponse {
             )
         } else if var buffer = response.body.buffer {
             let bytes = buffer.readBytes(length: buffer.readableBytes)!
-            self = APIGatewayResponse(
+            self = APIGateway.Response(
                 statusCode: AWSLambdaEvents.HTTPResponseStatus(code: response.status.code),
                 multiValueHeaders: headers,
                 body: String(base64Encoding: bytes),
                 isBase64Encoded: true
             )
         } else {
-            self = APIGatewayResponse(
+            self = APIGateway.Response(
                 statusCode: AWSLambdaEvents.HTTPResponseStatus(code: response.status.code),
                 multiValueHeaders: headers
             )
